614fc774c88fc488c8d830ec3229e899f78427be
impfundev <mail@ilhammaulana.me>
2024-06-18 10:51:26 +0700

    fix: image cache configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    minimumCacheTTL: 31536000,
+    deviceSizes: [320, 720],
     remotePatterns: [
       {
         protocol: "https",
